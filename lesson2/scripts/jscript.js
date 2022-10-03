e2259fb38df8d16281f23e514fbefc64038e79b2
EdgarJRM <edgarrodriguezm@hotmail.com>
2022-10-02 21:46:45 -0500

    Design Principles Document

---
+++ lesson2/scripts/jscript.js
@@ -0,0 +1,24 @@
+let date1 = document.querySelector("#currendate");
+
+try {
+	const options = {
+		day: "numeric",
+		month: "numeric",
+		year: "numeric"
+	};
+	date1.textContent = `${new Date().toLocaleDateString("en-UK", options)} ${mueveReloj()}`;
+} 
+catch (e) {
+	alert("Error with code or your browser does not support Locale");
+}
+
+function mueveReloj(){
+    momentoActual = new Date();
+    hora = momentoActual.getHours();
+    minuto = momentoActual.getMinutes();
+    segundo = momentoActual.getSeconds();
+
+    horaImprimible = hora + ":" + minuto + ":" + segundo;
+
+    return horaImprimible;    
+}
